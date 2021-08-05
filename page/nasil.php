75590eced7aa610b31a8e34436b5f85a8a4d5858
Ali Akman <thealico@gmail.com>
2021-08-05 20:22:27 +0300

    Finish

--- page/nasil.php
+++ page/nasil.php
@@ -19,13 +19,13 @@
 		
 		<h1>Nasıl terapi alabilirim?</h1>
 
-		<div class="row pt-80">
+		<div class="row row-sm-1 center pt-80">
 
-			<div class="col">
+			<div class="col mb-30-sm">
 				<img src="<?php echo path('img/nasil.jpg') ?>" />
 			</div>
 
-			<ul class="col pl-60">
+			<ul class="col col-sm-11 pl-60 pl-0-sm txt-xs-c">
 				<li>
 					<i>1</i>
 					<div>
@@ -58,18 +58,18 @@
 
 <section class="sec-navs">
 	<div>
-		<div class="con py-90">
+		<div class="con py-90 py-60-sm txt-sm-c">
 			<ul>
-				<li class="row middle between">
-					<div class="col-8">
+				<li class="row middle between center-sm mb-70">
+					<div class="col-8 col-sm-12 mb-15-sm">
 						<h3>Hazırsanız hemen randevunuzu alın</h3>
 						<p>Uzmanlarımızı ve hizmetlerimizi incelediyseniz ilk randevunuzu almanın zamanı geldi. Hala emin olamıyorsanız yukarıdaki iletişim formuyla kafanıza takılanları bize iletin!</p>
 					</div>
 					<a href="#" class="btn b-2">Hemen Randevu Al</a>
 				</li>
 
-				<li class="row middle between">
-					<div class="col-8">
+				<li class="row middle between center-sm">
+					<div class="col-8 col-sm-12 mb-15-sm">
 						<h3>Hazırsanız hemen randevunuzu alın</h3>
 						<p>Telefonla asistanımıza da ulaşabilirsiniz. Asistanımız ilk fırsatta bizimle irtibata geçmenizi sağlayacaktır.</p>
 					</div>
